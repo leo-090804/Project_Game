ab6d69bdef5147755009653aeab1a9f75bed00d6
leo-090804 <116019687+leo-090804@users.noreply.github.com>
2023-05-02 15:17:14 +0700

    Create flash.cpp

---
+++ flash.cpp
@@ -0,0 +1,30 @@
+#include "flash.h"
+
+using namespace std;
+
+void Flash::init(SDL_Renderer *renderer){
+    loadSprites(surface, texture, renderer, path);
+    alpha = 100;
+}
+
+void Flash::display(SDL_Renderer* renderer){
+    rect = {0,0,1280,720};
+    //SDL_GetTextureAlphaMod(texture, &alpha);
+    SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND);
+    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+    for(int i=0; i<20; i++){
+        SDL_SetTextureAlphaMod(texture, alpha);
+        SDL_RenderCopy(renderer, texture, NULL, &rect);
+        SDL_RenderPresent(renderer);
+    }
+}
+
+void Flash::displayNoAlpha(SDL_Renderer* renderer){
+    if(alpha>0) alpha-=10;
+    SDL_SetTextureAlphaMod(texture, alpha);
+    SDL_RenderCopy(renderer, texture, NULL, &rect);
+}
+
+void Flash::destroy(){
+    SDL_DestroyTexture(texture);
+}
