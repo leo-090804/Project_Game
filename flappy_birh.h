f559b36292c93d9f8abc2483b13cfb1b649d972d
leo-090804 <116019687+leo-090804@users.noreply.github.com>
2023-05-02 15:00:45 +0700

    Create flappy_birh.h

---
+++ flappy_birh.h
@@ -0,0 +1,90 @@
+#ifndef FLAPPY_BIRD_H
+#define FLAPPY_BIRD_H
+
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+#include <cstdint>
+
+#include "sdl_engine.h"
+#include "bird.h"
+#include "background.h"
+#include "base.h"
+#include "pipe.h"
+#include "scoreboard.h"
+#include "message.h"
+#include "coin.h"
+#include "music.h"
+#include "flash.h"
+#include "text.h"
+#include "selection.h"
+#include "saves.h"
+
+using namespace std;
+
+class FlappyBird
+{
+public:
+    void init();
+    void quit();
+    void reset();
+    void menu();
+    void getReady();
+    void gameLoop();
+    void gameOver();
+    void display();
+    void resInit();
+    void resGen();
+    void resDestroy();
+    void nextLevel();
+private:
+    int SCREEN_WIDTH = 1280;
+    int SCREEN_HEIGHT = 720;
+    string WINDOW_TITLE = "Flappy Bird";
+
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_Event event;
+
+    bool gameLose = false;
+    bool gameReset = false;
+    bool gameQuit = false;
+    bool menuLoop = true;
+    bool readyLoop = true;
+    bool level2 = false;
+    bool casual = false;
+    bool help = false;
+
+    int randNum;
+    int delay = 60;
+    int multiplier = 1;
+    int threshold = 4;
+    int frameNum = 1;
+
+    int mouseX;
+    int mouseY;
+
+    Bird bird;
+
+    Background background;
+    Base base;
+    SFX sfx;
+    Flash flash;
+    Message message;
+    Text text;
+    Selection selection;
+
+    Scoreboard scoreboard;
+    int score = 0;
+
+    Pipe pipe[7];
+    bool scoreStatus[7];
+
+    Coin coin[7];
+    bool coinStatus[7];
+    int coinRandom[7];
+
+    Saves saves;
+};
+
+#endif // FLAPPY_BIRD_H
