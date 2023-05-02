1be75da318aae2f1fc35621f963b5c154c0f2ed1
leo-090804 <116019687+leo-090804@users.noreply.github.com>
2023-05-02 14:57:52 +0700

    Create bird.h

---
+++ bird.h
@@ -0,0 +1,41 @@
+#ifndef BIRD_H
+#define BIRD_H
+
+#include "sdl_engine.h"
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+class Bird
+{
+public:
+    SDL_Rect dstrect;
+
+    void select();
+    void loadPNG(SDL_Renderer* renderer);
+    void initCasual();
+    void display(SDL_Renderer* renderer);
+    void update();
+    void keyUpdate();
+    void aniUpdate();
+    void statusUpdate(bool &close);
+    void collideBase(SDL_Rect rect1, SDL_Rect rect2, bool &lose);
+    void destroy();
+private:
+    const char* path = "sprites\\yellowBird.png";
+    SDL_Texture *texture = NULL;
+    SDL_Surface *surface = NULL;
+    SDL_Rect srcrect;
+    int flapStatus = 1; //1-up 2-mid 3-down
+    bool flapReversed = false;
+    int speed = 500;
+    int velocity = 0;
+    int delay = 0;
+    double angle = 0;
+    int hold = 0;
+    SDL_Point* center = NULL;
+    SDL_RendererFlip flip = SDL_FLIP_NONE;
+};
+
+#endif // BIRD_H
